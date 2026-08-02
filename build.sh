c1ffc040796ee2ab2dd6ae691f7101c050023adf
Icee12321 <devahmedebrahim@gmail.com>
2026-08-03 07:05:21 +0800

    structure

---
+++ build.sh
@@ -0,0 +1,2 @@
+
+gcc -o chip8.exe src/main.c -Iinclude -Llib -lraylib -lgdi32 -lwinmm
---
+++ src/main.c
@@ -0,0 +1,10 @@
+#include "../include/raylib.h"
+
+int main() {
+    InitWindow(600, 600, "chip8");
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        EndDrawing();
+    }
+}
